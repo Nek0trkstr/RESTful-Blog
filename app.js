0667bed148c37fafd3a27b6cfc444894b4208240
Michael Shebeko Admin <mshebeko@gmail.com>
2018-01-17 23:00:53 +0200

    Sanitizing and UI improvements

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require('express');
 app = express();
 bodyParser = require('body-parser');
 mongoose = require('mongoose');
+methodOverride = require("method-override");
+expressSanitizer = require('express-sanitizer');
 
 //APP CONFIG
 mongoose.connect('mongodb://localhost/restful_blog_app');
 app.set('view engine','ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
+
 
 //MONGOOSE SCHEMA CONFIG
 var blogSchema = new mongoose.Schema({
@@ -38,6 +43,7 @@ app.get('/blogs/new',function(req,res){
 });
 
 app.post('/blogs',function(req,res){
+	req.body.blog.body = req.sanitize(req.body.blog.body)
 	Blog.create(req.body.blog, function(err, newBlog){
 		if(err){
 			res.render('new')
@@ -53,6 +59,37 @@ app.get('/blogs/:id', function(req,res){
 			res.redirect('/blogs')
 		} else {
 			res.render('show',{blog: foundBlog})
+		}
+	})
+});
+
+app.get('/blogs/:id/edit', function(req,res){
+	Blog.findById(req.params.id,function(err,foundBlog){
+		if(err){
+			res.redirect('/blogs')
+		} else {
+			res.render('edit',{blog: foundBlog})
+		}
+	});
+});
+
+app.put('/blogs/:id', function(req,res){
+	req.body.blog.body = req.sanitize(req.body.blog.body)
+	Blog.findByIdAndUpdate(req.params.id,req.body.blog,function(err,updatedBlog){
+		if (err){
+			res.redirect('/blogs')
+		} else {
+			res.redirect('/blogs/'+req.params.id)
+		}
+	});
+});
+
+app.delete('/blogs/:id',function(req,res){
+	Blog.findByIdAndRemove(req.params.id,function(err){
+		if (err){
+			res.redirect('/blogs');
+		} else {
+			res.redirect('/blogs');
 		}
 	})
 });
